16289718ed56b3a327b68ce3354743e59eb1eabf
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-31 09:53:01 -0500

    fixed writefile

--- routes/notes.js
+++ routes/notes.js
@@ -9,41 +9,44 @@ const fs = require('fs');
 
 router.get('/', (req, res) => {
 
-    res.json(db);
+  res.json(db);
 });
 
 // POST Route for a new Note
 router.post('/', (req, res) => {
 
-        if (req.body) {
-            const { title, text } = req.body;
-            const newNote = {
-                title: title,
-                text: text,
-                note_id: uuidv4()
-            };     
-
-            readAndAppend(res, newNote, './db/db.json');
-            res.json(`Note added successfully 🚀`);
-        } else {
-            res.status(500).error('Error in adding note');
-        }
-    });
-
+  if (req.body) {
+    const { title, text } = req.body;
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4()
+    };
     
-const readAndAppend = (res, content, file) => {
-    fs.readFileSync(file, 'utf8', (err, data) => {
-      if (err) {
-        res.json(`failed to write note`);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);   
-        fs.writeFileSync(file, JSON.stringify(parsedData, null, 4));
-        res.json(`new note created`);
-      }
-    });
-   
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.status(500).error('Error in adding note');
+  }
+});
+
+
+const readAndAppend = (content, file) => {
+  
+  fs.readFile(file, 'utf8', (err, data) => {
+    console.log('starting to create the file....');
+    if (err) {
+      console.log('error creating file....');
+    } else {
+      console.log('creating file....');
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      const json = JSON.stringify(parsedData, null, 4);
+      fs.writeFile(file, json, 'utf8', () => {console.log('file successfully created')});
+    }
+  });
+};
 
 module.exports = router;
 
-// BONUS: DELETE NOTE HERE!!!!
+// BONUS: Add delete NOTES
